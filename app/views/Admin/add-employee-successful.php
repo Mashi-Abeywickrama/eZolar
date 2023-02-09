c8ddecd507d1d361d2ff7053ba2931480126c7e6
Amashi Abeywickrama <91023322+Mashi-Abeywickrama@users.noreply.github.com>
2023-02-09 15:34:05 +0530

    Revert "Salesperson operations"

--- app/views/Admin/add-employee-successful.php
+++ app/views/Admin/add-employee-successful.php
@@ -23,21 +23,15 @@
         </div>
         <div class="sidebar-link-container-group">
             <div class="sidebar-link-container-top">
-                <a href="/ezolar/Employee">
-                    <div class="sidebar-link-container-selected">
+                <a href="/ezolar/Employee"><div class="sidebar-link-container-selected">
                     Employees
-                    </div>
-                </a>
-                <a href=/ezolar/Package>
-                    <div class="sidebar-link-container">
-                        Packages
-                    </div>
-                </a>
-                <a href=/ezolar/Product>
-                    <div class="sidebar-link-container">
-                        Products
-                    </div>
-                </a>
+                </div></a>
+                <div class="sidebar-link-container">
+                    Packages
+                </div>
+                <div class="sidebar-link-container">
+                    Products
+                </div>
                 <div class="sidebar-link-container">
                     Reports 
                 </div>
@@ -46,7 +40,7 @@
             <div class="sidebar-link-container-bottom">
                 <a href="/ezolar/AdminViewProfile"><div class="sidebar-link-container">
                     Profile
-                </div>
+                </div></a>
                 <div class="sidebar-link-container">
                     Settings
                 </div>
--- app/views/Admin/engineers.php
+++ app/views/Admin/engineers.php
@@ -16,7 +16,7 @@
     <title>My Projects</title>
 </head>
 <body>
-    <div class="sidebar">
+<div class="sidebar">
         <div class="sidebar-heading">
             <b>Admin Dashboard</b>
         </div>
@@ -25,16 +25,12 @@
                 <a href="/ezolar/Employee"><div class="sidebar-link-container-selected">
                     Employees
                 </div></a>
-                <a href=/ezolar/Package>
-                    <div class="sidebar-link-container">
-                        Packages
-                    </div>
-                </a>
-                <a href=/ezolar/Product>
-                    <div class="sidebar-link-container">
-                        Products
-                    </div>
-                </a>
+                <div class="sidebar-link-container">
+                    Packages
+                </div>
+                <div class="sidebar-link-container">
+                    Products
+                </div>
                 <div class="sidebar-link-container">
                     Reports 
                 </div>
@@ -43,7 +39,7 @@
             <div class="sidebar-link-container-bottom">
                 <a href="/ezolar/AdminViewProfile"><div class="sidebar-link-container">
                     Profile
-                </div>
+                </div></a>
                 <div class="sidebar-link-container">
                     Settings
                 </div>
