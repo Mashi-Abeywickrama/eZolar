e0d3e6432c6f484391ca2da59e54d1f395d4749b
Dinuk-DeSilva <dinuklolitha@gmail.com>
2023-04-10 18:26:02 +0530

    Engineer Calendar

--- app/libraries/Calendar.php
+++ app/libraries/Calendar.php
@@ -3,6 +3,7 @@
 class Calendar extends DateTime{
     protected $year;
     protected $month;
+    protected $monthName;
     protected $weekDays = [
         'Mon','Tue','Wed','Thu','Fri','Sat','Sun'
     ];
@@ -28,10 +29,31 @@ class Calendar extends DateTime{
     public function getweeks(){
         return $this->weeks;
     }
+    public function getMonthName(){
+        return $this->monthName;
+    }
+    public function getNextMonth(){
+        $nextMonth = [$this->getMonth()+1,$this->getYear()];
+        if ($nextMonth[0]>12){
+            $nextMonth = [1,$nextMonth[1]+1];
+        }
+        return $nextMonth;
+
+    }
+    public function getPrevMonth(){
+        $prevMonth = [$this->getMonth()-1,$this->getYear()];
+        if ($prevMonth < 1){
+            $prevMonth = [1,$prevMonth[1]-1];
+        }
+        return $prevMonth;
+
+    }
     public function create(){
         $date = $this->setDate($this->getYear(),$this->getMonth(),1);
         $daysInMonth = $date->format('t');
-        $startday = $date->format('N');
+        $startday = $date->format('N')-1;
+        $this->monthName = $date->format('F');
+        
 
         $days = array_fill(0, $startday,'');
 
@@ -40,6 +62,7 @@ class Calendar extends DateTime{
         }
 
         $this->weeks = array_chunk($days,7);
+        $this->weeks[count($this->weeks)-1] = array_pad($this->weeks[count($this->weeks)-1],7,'');
 
 
     }
